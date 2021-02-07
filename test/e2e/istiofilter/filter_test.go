cc717741efd154f9af6f3143c89452efe4876fbb
cocotyty <cocotyty@sina.com>
2021-02-07 17:15:14 +0800

    add OUTLIER_DETECTION e2e test
    Signed-off-by: cocotyty <cocotyty@sina.com>

--- test/e2e/istiofilter/filter_test.go
+++ test/e2e/istiofilter/filter_test.go
@@ -10,7 +10,7 @@ import (
 func TestMain(m *testing.M) {
 	utils.EnableVerbose()
 	deployBookInfo()
-	//defer clearBookInfo()
+	defer clearBookInfo()
 	m.Run()
 }
 
@@ -49,6 +49,39 @@ func TestPatchVirtualService(t *testing.T) {
 	}
 	utils.Kubectl().DeleteByFile("./testdata/mirror.yaml")
 	mirror, err = utils.Kubectl().Get("VirtualService").ByName("productpage").ObjectField(".spec.http[0].mirror")
+	if err != nil {
+		t.Fatal(err)
+	}
+	wanted = ""
+	if mirror != wanted {
+		t.Fatalf("excepted mirror: %s but got: %s", wanted, mirror)
+	}
+}
+
+func TestPatchDestination(t *testing.T) {
+	utils.Kubectl().Apply("./testdata/outlier_detection.yaml")
+	time.Sleep(time.Millisecond * 100)
+	mirror, err := utils.Kubectl().Get("DestinationRule").ByName("productpage").ObjectField(".spec.trafficPolicy.outlierDetection")
+	if err != nil {
+		t.Fatal(err)
+	}
+	wanted := "{\"baseEjectionTime\":\"180s\",\"consecutiveErrors\":1,\"interval\":\"1s\",\"maxEjectionPercent\":100}"
+	if mirror != wanted {
+		t.Fatalf("excepted mirror: %s but got: %s", wanted, mirror)
+	}
+	utils.Kubectl().Get("DestinationRule").ByName("reviews").YAML()
+
+	mirror, err = utils.Kubectl().Get("DestinationRule").ByName("reviews").ObjectField(".spec.subsets[0].trafficPolicy.outlierDetection")
+	if err != nil {
+		t.Fatal(err)
+	}
+	wanted = "{\"baseEjectionTime\":\"180s\",\"consecutiveErrors\":1,\"interval\":\"1s\",\"maxEjectionPercent\":100}"
+	if mirror != wanted {
+		t.Fatalf("excepted mirror: %s but got: %s", wanted, mirror)
+	}
+
+	utils.Kubectl().DeleteByFile("./testdata/outlier_detection.yaml")
+	mirror, err = utils.Kubectl().Get("DestinationRule").ByName("productpage").ObjectField(".spec.trafficPolicy.outlierDetection")
 	if err != nil {
 		t.Fatal(err)
 	}
